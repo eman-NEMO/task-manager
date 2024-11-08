c804c5ecd3225f2b4ff26f129bc969e389851941
eman-NEMO <emanelkaser@gmail.com>
2024-11-08 21:00:27 +0200

    Update index.tsx

--- src/pages/tasks/index.tsx
+++ src/pages/tasks/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import TaskTemple from '@/components/templates/TaskTemplate'
-import { TaskProvider } from '@/Context/TaskContext';
+
 export default function index() {
   return (
-    // <TaskProvider>
+ 
     <TaskTemple/>
-    // </TaskProvider>
+    
   )
 }
